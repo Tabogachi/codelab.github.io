8d7a16bd5496864e0f6cd4f7c6484b08d666e3a6
Tabogachi <70734556+Tabogachi@users.noreply.github.com>
2020-09-24 10:10:20 +1200

    Website Update 2

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function firstPropmt() {
     var mountain = window.prompt("What is your mountain?");
     document.getElementById("pepe2").innerHTML = "Ko " + mountain + " Te Maunga";
+    var river = window.prompt("What is your river?");
+    document.getElementById("pepe3").innerHTML = "Ko " + river + " Te Awa";
+    var city = window.prompt("What is your city?");
+    document.getElementById("pepe4").innerHTML = "Nō " + city + " Ahua";
+    var family = window.prompt("Who is your family?");
+    document.getElementById("pepe5").innerHTML = "Ko " + family + " Tōku Whānau";
+    var name = window.prompt("What is your name?");
+    document.getElementById("pepe6").innerHTML = "Ko " + name + " Tōku Ingoa";
+}
+
+function printPropmt(){
+    var print = window.print()
 }
